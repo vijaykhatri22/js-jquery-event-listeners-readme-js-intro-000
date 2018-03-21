042a6dc26411d23e27f8552f7c45415d5b594074
vijay khatri <vijaykhatri22@gmail.com>
2018-03-21 10:17:48 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,7 +2,10 @@
 function getIt(){
   $('p').on("click",()=>alert("Hey!"))
 }
-
+function frameIt(){
+  $('img').on("load"()=> $('img').addClass('tasty'))
+  
+}
 $(document).ready(function(){
 
 // call functions here
