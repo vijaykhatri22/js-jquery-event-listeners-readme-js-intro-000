257703fc444901ea24de32d1987934cf02816d35
vijay khatri <vijaykhatri22@gmail.com>
2018-03-21 10:41:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,6 +12,11 @@ function pressIt(){
       }
   } )
 }
+function submitIt(){
+  $('form').on("submit",()=>{
+    alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
 
 // call functions here
