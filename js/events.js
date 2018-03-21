e25e484cc5c83005050a65c3fe9ed40940ae85e9
vijay khatri <vijaykhatri22@gmail.com>
2018-03-21 10:19:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,7 +3,7 @@ function getIt(){
   $('p').on("click",()=>alert("Hey!"))
 }
 function frameIt(){
-  $('img').on("load"()=> $('img').addClass('tasty'))
+  $('img').on("load",()=> $('img').addClass('tasty'))
 
 }
 $(document).ready(function(){
