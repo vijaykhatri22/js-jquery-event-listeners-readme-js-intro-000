bfe2f3f843916fcc6c2cd9cc082f7f4a71cdfecc
vijay khatri <vijaykhatri22@gmail.com>
2018-03-21 10:42:53 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
 //define functions here
-
+function getIt(){
+  $('p').on("click",()=>alert("Hey!"))
+}
+function frameIt(){
+  $('img').on('load',() => { $('img').addClass('tasty')} )
+}
+function pressIt(){
+  $('#typing').on("keydown", (key)=>{
+      if(key.which == 71 ){
+        alert("g is pressed")
+      }
+  } )
+}
+function submitIt(){
+  $('form').on("submit",()=>{
+    alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
